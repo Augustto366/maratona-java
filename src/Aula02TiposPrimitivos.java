c01b25cf935163dd3943c47139b75770eefd70e2
Augustto366 <augustodasilva938@gmail.com>
2025-06-23 19:53:48 -0300

    aulas

--- src/Aula02TiposPrimitivos.java
+++ src/Aula02TiposPrimitivos.java
@@ -2,6 +2,6 @@ public class Aula02TiposPrimitivos {
     public static void main (String[] args) {
         // int, double, float, char, byte, short, long, boolean
         int idade = 10;
-        System.out.println("A idade é: " + idade);
+        System.out.println("A idade é: " + idade + " anos.");
     }
 }
