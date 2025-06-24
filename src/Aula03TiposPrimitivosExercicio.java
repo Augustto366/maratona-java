7ddcb952b4cdd582bb081421d98675e125c0aece
Augustto366 <augustodasilva938@gmail.com>
2025-06-24 00:24:34 -0300

    aula4

--- src/Aula03TiposPrimitivosExercicio.java
+++ src/Aula03TiposPrimitivosExercicio.java
@@ -1,3 +1,11 @@
 public class Aula03TiposPrimitivosExercicio {
+    public static void main (String[] args) {
+        String nome = "Marcelo";
+        int idade = 22;
+        String endereco = "Bairro X - Rua alguma coisa, 123";
+        double salario = 1500D;
+        String dataRecebimento = "03/05/2013";
 
+        System.out.println("Eu " + nome + ", morando no endereço: " + endereco + ", confirmo que recebi o salário de " + salario + " na data " + dataRecebimento + ".");
+    }
 }
---
+++ src/Aula04Operadores.java
@@ -0,0 +1,7 @@
+public class Aula04Operadores {
+    public static void main (String[] args) {
+        double numero01 = 10;
+        double numero02 = 20;
+        System.out.println(numero01 / numero02);
+    }
+}
