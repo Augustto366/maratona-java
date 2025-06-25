02c33a6013d65324badcbfa6a33e496b882496af
Augustto366 <augustodasilva938@gmail.com>
2025-06-24 23:42:19 -0300

    aula 4

--- src/Aula04Operadores.java
+++ src/Aula04Operadores.java
@@ -3,5 +3,43 @@ public class Aula04Operadores {
         double numero01 = 10;
         double numero02 = 20;
         System.out.println(numero01 / numero02);
+
+
+        //%
+        int resto = 20 % 7;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        System.out.println("É maior? " +isDezMaiorQueVinte);
+
+        // && (AND) || (or) ! (NOT)
+        int idade = 35;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario <= 3381;
+        System.out.println(isDentroDaLeiMaiorQueTrinta + " " + isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupança = 10000;
+        float valorPlaystation = 5000F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupança > valorPlaystation;
+        System.out.println(isPlaystationCincoCompravel);
+
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        // ++ --
+        int contador = 0;
+        contador += 1;
+        contador++;
+        contador--;
+        System.out.println(contador);
     }
 }
